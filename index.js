f4a21bc9352f48100a98e81e154fde8bfc9b64b9
Lavanya Ammi Chandrashekara <lavanya.ammic@gmail.com>
2017-10-28 22:46:06 -0700

    few changes

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const path = require('path');
 
-app.get('/', function (req, res) {
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', function(req, res) {
+	console.log("/");
+    res.sendFile(path.join(__dirname + 'client/build/index.html'));
+});
+
+app.get('/hello', function (req, res) {
+	console.log("/hello");
   res.send('Hello World!')
 })
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
 const port = process.env.PORT || 3001;
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`)
